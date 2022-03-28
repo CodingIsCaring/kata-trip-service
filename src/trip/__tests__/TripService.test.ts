2fe8cd0294951bcaf03b04de6cd24c312b9c00d4
Maria Soria <maria.it3@gmail.com>
2022-03-28 20:01:30 +0100

    Add scaffolding tests

--- src/trip/__tests__/TripService.test.ts
+++ src/trip/__tests__/TripService.test.ts
@@ -1,7 +1,54 @@
 import 'jest';
+import TripService from '../TripService';
+import User from '../../user/User';
+import UserSession from '../../user/UserSession';
+import TripDAO from '../TripDAO';
+import Trip from '../Trip';
 
-describe('TripServiceShould', () => {
-  it('...', () => {
-    expect(4 + 4).toBe(9);
+describe('TripService', () => {
+  const tripService: TripService = new TripService();
+
+  describe('gets trips when user is empty', () => {
+    it('and another user must be logged', () => {
+      const user: User = new User();
+      UserSession.getLoggedUser = () => new User();
+
+      const tripsByUser = tripService.getTripsByUser(user);
+
+      expect(tripsByUser).toMatchSnapshot();
+    });
+    it.todo('not logged user needed');
+  });
+
+  describe('get trips when user is not empty', () => {
+    describe('and another user must be logged', () => {
+      const loggedUser: User = new User();
+
+      it('and must be friend with the initial user', () => {
+        const userWithTrip: User = new User();
+        userWithTrip.addTrip(new Trip());
+        UserSession.getLoggedUser = () => loggedUser;
+        userWithTrip.addFriend(loggedUser);
+        TripDAO.findTripsByUser = () => {
+          const trips: Trip[] = [];
+          trips.push(new Trip());
+          return trips;
+        };
+
+        const tripsByUser = tripService.getTripsByUser(userWithTrip);
+
+        expect(tripsByUser).toMatchSnapshot();
+      });
+
+      it('and they are not friends', () =>{
+        const userWithTrip: User = new User();
+        UserSession.getLoggedUser = () => loggedUser;
+
+        const tripsByUser = tripService.getTripsByUser(userWithTrip);
+
+        expect(tripsByUser).toMatchSnapshot();
+      });
+    });
+    it.todo('not logged user needed');
   });
 });

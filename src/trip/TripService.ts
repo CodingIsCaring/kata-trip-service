9b9ed56aa6d0a5dfc3dadbc541b18391a72581e8
Maria Soria <maria.it3@gmail.com>
2022-03-28 20:04:36 +0100

    reverse if condition

--- src/trip/TripService.ts
+++ src/trip/TripService.ts
@@ -10,21 +10,20 @@ export default class TripService {
     const loggedUser: User = UserSession.getLoggedUser();
     let isFriend: boolean = false;
 
-    if (loggedUser != null) {
-      for (const friend of user.getFriends()) {
-        if (friend === loggedUser) {
-          isFriend = true;
-          break;
-        }
-      }
-
-      if (isFriend) {
-        tripList = TripDAO.findTripsByUser(user);
-      }
-
-      return tripList;
-    } else {
+    if (loggedUser == null) {
       throw new UserNotLoggedInException();
     }
+    for (const friend of user.getFriends()) {
+      if (friend === loggedUser) {
+        isFriend = true;
+        break;
+      }
+    }
+
+    if (isFriend) {
+      tripList = TripDAO.findTripsByUser(user);
+    }
+
+    return tripList;
   }
 }
